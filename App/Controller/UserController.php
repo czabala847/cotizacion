bd0dc6479996909c172aaca72f03e3359afeb502
Carlos Zabala <czabala847@gmail.com>
2020-05-15 20:32:11 -0500

    Activando funcionalidades de modificar usuarios

--- App/Controller/UserController.php
+++ App/Controller/UserController.php
@@ -4,25 +4,28 @@ require_once("../Model/User.php");
 
 $user = new User();
 
+//Parte de activar y desactivar usuarios
 if (isset($_POST["modify"])) {
 
   $result = $user->changeStatus($_GET["id"]);
   echo json_encode($result);
 }
+//Parte de modificar usuarios
+else {
 
-// $id = $_POST["id"];
-// $name = $_POST["nombre"];
-// $email = $_POST["correo"];
+  $id = $_POST["id"];
+  $name = $_POST["nombre"];
+  $email = $_POST["correo"];
 
-// //Validar si se quiere actualizar tambien la contraseña
-// if (isset($_POST["password"])) {
-//   $password = $_POST["password"];
-//   $resultUpdate = $user->updateUser($id, $name, $email, $password);
-// } else {
-//   $resultUpdate = $user->updateUser($id, $name, $email);
-// }
+  //Validar si se quiere actualizar tambien la contraseña
+  if (isset($_POST["password"])) {
+    $password = $_POST["password"];
+    $resultUpdate = $user->updateUser($id, $name, $email, $password);
+  } else {
+    $resultUpdate = $user->updateUser($id, $name, $email);
+  }
 
 
 
-// echo json_encode($resultUpdate);
-// echo json_encode($password);
+  echo json_encode($resultUpdate);
+}
