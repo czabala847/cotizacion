6c0dd7e3d8a3081cb2f01857932517d2f061577e
Carlos Zabala <czabala847@gmail.com>
2020-08-13 15:28:46 -0500

    Completando sidebar de la dashboard

--- Assets/js/Dashboard/SideBar.js
+++ Assets/js/Dashboard/SideBar.js
@@ -1,9 +1,29 @@
 const $iconMenu = document.querySelector("#sidebar-menu-icon");
 const $appContainer = document.querySelector("#app");
+const $menuDropDown = document.querySelectorAll("[data-toggle='drop-down']");
 // const $sidebarHeader =
 
 if ($iconMenu) {
   $iconMenu.addEventListener("click", () => {
     $appContainer.classList.toggle("sidebar-toggled");
+  });
+}
+
+if ($menuDropDown) {
+  $menuDropDown.forEach((itemMenu) => {
+    itemMenu.addEventListener("click", (e) => {
+      e.preventDefault();
+      const $parentElement = itemMenu.parentNode;
+
+      $parentElement.classList.toggle("expanded");
+      const $iconAngle = itemMenu.querySelector("#icon-angle");
+
+      // debugger;
+      if ($parentElement.classList.contains("expanded")) {
+        $iconAngle.classList.replace("fa-angle-right", "fa-angle-down");
+      } else {
+        $iconAngle.classList.replace("fa-angle-down", "fa-angle-right");
+      }
+    });
   });
 }
--- View/Templates/Sidebar.php
+++ View/Templates/Sidebar.php
@@ -2,14 +2,33 @@
     <div class="sidebar-container">
         <div class="sidebar-header">
             <figure class="sidebar-header__logo">
-                <h3 class="sidebar-header__logo--title"><a class="link-normal" href="<?= getUrlBase() ?>dashboard">LOGO</a></h3>
+                <h3 class="sidebar-header__logo--title title-logo"><a class="link-normal" href="<?= getUrlBase() ?>dashboard">LOGO</a></h3>
                 <div class="sidebar-header__logo--circle"></div>
             </figure>
             <i id="sidebar-menu-icon" class="sidebar-header__icon fas fa-bars"></i>
         </div>
         <nav class="sidebar__menu">
-            <ul>
-                <li class="sidebar__menu--item"><a class="link-normal" href=""><i class="fas fa-users"></i><span>Usuarios</span></a></li>
+            <ul class="list-normal">
+                <li class="sidebar__menu--item"><a class="link-normal" href="<?= getUrlBase() ?>dashboard"><i class="fas fa-home"></i><span>Dashboard</span></a></li>
+                <li class="sidebar__menu--item menu-dropdown">
+                    <a class="link-normal" href="" data-toggle="drop-down">
+                        <i class="fas fa-users"></i>
+                        <span>Usuarios</span>
+                        <i id="icon-angle" class="fas fa-angle-right"></i>
+                    </a>
+                    <ul class="list-normal sidebar__submenu">
+                        <li class="sidebar__submenu--item">
+                            <a href="" class="link-normal">
+                                <span>Lista usuarios</span>
+                            </a>
+                        </li>
+                        <li class="sidebar__submenu--item">
+                            <a href="" class="link-normal">
+                                <span>Roles</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
                 <li class="sidebar__menu--item"><a class="link-normal" href="<?= getUrlBase() ?>cotizacion"><i class="fas fa-file-invoice-dollar"></i><span>Cotizaciones</span></a></li>
                 <li class="sidebar__menu--item"><a class="link-normal" href="<?= getUrlBase() ?>user/logout"><i class="fas fa-sign-out-alt"></i><span>Salir</span></a></li>
             </ul>
