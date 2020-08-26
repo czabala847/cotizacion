edfaaa2be60949ee396c5f387cd5702e753bfb9c
Carlos Zabala <czabala847@gmail.com>
2020-08-26 11:18:23 -0500

    Terminando diseño de la actualizacion de usuarios

--- View/User/editarUsuario.php
+++ View/User/editarUsuario.php
@@ -5,7 +5,6 @@
     <div class="container">
         <div class="user-container">
             <h2 class="title-dashboard">Editar usuario</h2>
-
             <div class="user-edit">
                 <div class="user-edit__header">
                     <p><?= $dataPage["user"]["nombre"]; ?></p>
@@ -17,34 +16,36 @@
                 </div>
                 <!-- ======= FORMULARIO EDITAR USUARIO============= -->
                 <form id="formUpdate" class="user-edit__form" autocomplete="off">
+                    <h3>Información general</h3>
                     <input type="hidden" name="id" value="<?= $dataPage['user']['id'] ?>" />
-                    <div class="form-group">
-                        <h3>Información general</h3>
-
-                        <div class="form-group cl-2">
-                            <label for="name"><span>Nombre</span>
-                                <input class="editUser__form--field" type="text" name="nombre" id="name" value="<?= $dataPage['user']['nombre'] ?>" required />
-                            </label>
-                            <label for="email"><span>Correo</span>
-                                <input class="editUser__form--field" type="email" name="correo" id="email" value="<?= $dataPage['user']['correo'] ?>" required />
-                            </label>
+                    <div class="form-group fg-2">
+                        <div class="form-group__group">
+                            <label for="name">Nombre</label>
+                            <input type="text" name="nombre" id="name" value="<?= $dataPage['user']['nombre'] ?>" required />
                         </div>
-
-                        <label><span>Seleccionar perfil de usuario</span>
-                            <select class="editUser__form--field" name="comboRol" id="comboRol">
-                                <option value="1">Administrador</option>
-                                <option value="2">Estandar</option>
-                            </select>
-                        </label>
-
-                    </div>
-                    <div class="form-group">
-                        <div class="form-group cl-2">
-                            <h3>Cambiar contraseña</h3><input type="checkbox" name="changePass" id="changePass" value="Si" />
+                        <div class="form-group__group">
+                            <label for="email">Correo</label>
+                            <input type="email" name="correo" id="email" value="<?= $dataPage['user']['correo'] ?>" required />
                         </div>
-                        <label for="pw1"><span>Contraseña</span><input class="editUser__form--field" type="password" name="password" id="pw1" disabled /></label>
-                        <label for="pw2"><span>Confirmar contraseña</span><input class="editUser__form--field" type="password" name="password2" id="pw2" disabled /></label>
                     </div>
+
+                    <label>Seleccionar perfil de usuario</label>
+                    <select name="comboRol" id="comboRol">
+                        <option value="1">Administrador</option>
+                        <option value="2">Estandar</option>
+                    </select>
+
+
+                    <div class="form-group">
+                        <div class="form-group__group textpass">
+                            <label class="title-dashboard-secondary" for="changePass">Cambiar contraseña</label>
+                            <input type="checkbox" name="changePass" id="changePass" value="Si" />
+                        </div>
+                    </div>
+
+                    <label for="pw1"><span>Contraseña</span><input type="password" name="password" id="pw1" disabled /></label>
+                    <label for="pw2"><span>Confirmar contraseña</span><input type="password" name="password2" id="pw2" disabled /></label>
+
                     <div class="form-login__btn">
                         <input class="btn btn--primary" type="submit" id="btnEnviar" value="Enviar">
                         <i id="icon-loading" class="fa fa-spinner fa-spin hidden-element"></i>
@@ -52,8 +53,6 @@
                 </form>
 
             </div>
-
-
         </div>
     </div>
 </section>
